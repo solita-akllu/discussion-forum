01289007414f01e79b057559d2bc9efcbd380f96
solita-akllu <aleksi.kalliokoski@solita.fi>
2024-02-01 15:23:18 +0200

    Add react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "./views/Login";
 
 const App = () => {
   return (
     <div className="h-screen">
-      <LoginPage />
+      <Routes>
+        <Route path="*" element={<LoginPage />} />
+      </Routes>
     </div>
   );
 };
