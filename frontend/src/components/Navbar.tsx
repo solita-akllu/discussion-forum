97861988fc6b307f84611b61bc0d662e972cf996
solita-akllu <aleksi.kalliokoski@solita.fi>
2024-02-09 13:40:44 +0200

    Show logged user info

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import { useAuthContext } from "../contexts/AuthContext";
 
 export const Navbar = () => {
-  const { logout } = useAuthContext();
+  const { user, logout } = useAuthContext();
 
   return (
-    <div className="flex w-screen justify-end bg-gray-400 p-4">
+    <div className="flex w-screen items-center justify-between bg-gray-400 p-4">
+      <span className="text-xl font-bold">Logged in as: {user?.name}</span>
       <button
         onClick={logout}
         className="mr-5 rounded-md bg-gray-500 p-2 hover:bg-gray-600"
